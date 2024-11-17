fcfad32346b8de67287223ebf3bf1ffc977a1a3a
Siddharth Das <66112421+eyepatch-3098@users.noreply.github.com>
2024-11-17 22:31:02 +0000

    tailwind visible

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -17,7 +17,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -127,11 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / "hello_world" / "static",
+    BASE_DIR / "static",
 ]
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "hello_world" / "staticfiles"
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "hello_world" / "media"
