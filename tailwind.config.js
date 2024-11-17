58554507619953f053cd68154d60bad59e487f3b
Siddharth Das <66112421+eyepatch-3098@users.noreply.github.com>
2024-11-17 21:15:17 +0000

    installed tailwind and refactored files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './books/templates/**/*.html',
+    './**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
